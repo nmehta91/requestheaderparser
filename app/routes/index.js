0bebced2488b9d79bb22723f780c95ea83585f7e
Nitish Mehta <nmehta91@ucla.edu>
2016-02-04 09:17:19 +0000

    Final working commit

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,57 +1,20 @@
 'use strict';
 
 var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
 
-module.exports = function (app, passport) {
 
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
+module.exports = function (app) {
 
-	var clickHandler = new ClickHandler();
-
-	app.route('/')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/index.html');
-		});
-
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
-		});
-
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
-
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
-
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
+	app.get('/', function(req, res) {
+		var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;	
+		var user_agent = req.headers['user-agent'].split(')')[0].split('(')[1];
+		var language = req.headers["accept-language"].split(',')[0];
+		
+		var obj = {
+			"ipaddress": ip,
+			"language" : language,
+			"software": user_agent,
+		};
+		res.send(JSON.stringify(obj));
+	});
 };
